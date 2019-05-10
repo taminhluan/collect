2765602664135c950b5484f83142e5775f4114cd
Penar Musaraj <pmusaraj@gmail.com>
2019-05-09 21:20:17 -0400

    Update strings

--- kobo-string-replace.js
+++ kobo-string-replace.js
@@ -1,29 +1,22 @@
 // Replace string occurrences with their Kobo equivalents
-// run "npm install"
+// run "npm install replace"
 // and then "node string-replace.js"
 
 var replace = require("replace");
 
 replace({
-    regex: "<string name=\"main_menu_details\">.*<",
-    replacement: "<string name=\"main_menu_details\">Part of KoBoToolbox<",
-    paths: ['collect_app/src/main/res'],
+    regex: '<string name="main_menu_details">.*<',
+    replacement: '<string name="main_menu_details">Part of KoBoToolbox<',
+    paths: ["collect_app/src/main/res"],
     recursive: true,
     silent: true
 });
 
 replace({
-    regex: "<string name=\"click_to_web\">.*<",
-    replacement: "<string name=\"click_to_web\">KoBoCollect is part of KoBoToolbox (kobotoolbox.org)\\n\\nKoBoCollect is based on the OpenDataKit<",
-    paths: ['collect_app/src/main/res'],
-    recursive: true,
-    silent: true
-});
-
-replace({
-    regex: "https://opendatakit.org",
-    replacement: "http://www.kobotoolbox.org",
-    paths: ['collect_app/src/main/java/org/koboc/collect/android/activities/AboutActivity.java'],
+    regex: '<string name="click_to_web">.*<',
+    replacement:
+        '<string name="click_to_web">KoBoCollect is part of KoBoToolbox (kobotoolbox.org)\\n\\nKoBoCollect is based on the OpenDataKit<',
+    paths: ["collect_app/src/main/res"],
     recursive: true,
     silent: true
 });
@@ -31,7 +24,7 @@ replace({
 replace({
     regex: "ODK Aggregate",
     replacement: "KoBoToolbox",
-    paths: ['collect_app/src/main/res'],
+    paths: ["collect_app/src/main/res"],
     recursive: true,
     silent: true
 });
@@ -39,33 +32,23 @@ replace({
 replace({
     regex: "ODK Collect",
     replacement: "KoBoCollect",
-    paths: ['collect_app/src/main/res'],
+    paths: ["collect_app/src/main/res"],
     recursive: true,
     silent: true
 });
 
 replace({
-    regex: 'Open Data Kit .ODK.',
+    regex: "Open Data Kit .ODK.",
     replacement: "KoBoToolbox",
-    paths: ['collect_app/src/main/res'],
+    paths: ["collect_app/src/main/res"],
     recursive: true,
     silent: true
 });
 
 replace({
-    regex: 'ODK',
+    regex: "ODK",
     replacement: "KoBoToolbox",
-    paths: ['collect_app/src/main/res'],
+    paths: ["collect_app/src/main/res"],
     recursive: true,
     silent: true
 });
-
-// This is likely too broad
-// replace({
-//     regex: "ODK",
-//     replacement: "KoboToolbox",
-//     paths: ['collect_app/src/main/res'],
-//     recursive: true,
-//     silent: true
-// });
-
